fdb5689732284a585f50553cbfe93d9f5481bd23
wHoIsDReAmer <lcw060403@gmail.com>
2025-12-05 14:14:26 +0900

    feat: add dependency; add simple entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+mod gui;
+
+use crate::gui::Counter;
+
+#[tokio::main]
+async fn main() {
+    let _ = iced::run("foo", Counter::update, Counter::view);
 }
